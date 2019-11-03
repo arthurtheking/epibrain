ebbec57cf83f7c54137c27001bacfd00e6dc5dc3
Arthur <bardartur@gmail.com>
2019-11-03 13:25:13 +0200

    Beta 3

--- dist/swiper.min.f296f991.js
+++ dist/swiper.min.f296f991.js
@@ -4338,7 +4338,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58361" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62493" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
